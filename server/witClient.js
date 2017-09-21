234b59327f2e5c8a17098ffff799c6ce3edb07bf
Nikhil Garg <nikhilgarg72@gmail.com>
2017-09-21 17:00:39 +0530

    Refactored witClient

--- server/witClient.js
+++ server/witClient.js
@@ -2,34 +2,32 @@
 
 const request = require('superagent');
 
-function handleWitResponse(res){
-    //console.log(res);
+function handleWitResponse(res) {
     return res.entities;
 }
 
-module.exports = function witClient(token){
-    const ask = function ask(message, cb){
-
-        request.get('https://api.wit.ai/message')
-        .set('Authorization', 'Bearer ' + token)
-        .query({v: '20/09/2017'})
-        .query({q: message})
-        .end((err, response) => {
-            if(err)
-                return cb(err);
-
-            if(response.statusCode != 200){
-                return cb('Expected status 200 but got ' + response.statusCode);
-            }
-
-            const witResponse = handleWitResponse(response.body);
-            cb(null, witResponse);
-        });
-        // console.log('ask: ' + message);
-        // console.log('token: ' + token);
+class WitClient {
+    constructor(token) {
+        this._token = token;
     }
 
-    return {
-        ask: ask
+    ask(message, cb) {
+        request.get('https://api.wit.ai/message')
+            .set('Authorization', 'Bearer ' + this._token)
+            .query({ v: '20/09/2017' })
+            .query({ q: message })
+            .end((err, response) => {
+                if (err)
+                    return cb(err);
+
+                if (response.statusCode != 200) {
+                    return cb('Expected status 200 but got ' + response.statusCode);
+                }
+
+                const witResponse = handleWitResponse(response.body);
+                cb(null, witResponse);
+            });
     }
 }
+
+module.exports = WitClient;
