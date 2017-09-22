6697e7f8a106fcb9328162f403ae55fa702b5505
Nikhil Garg <nikhilgarg72@gmail.com>
2017-09-23 01:12:01 +0530

    updated pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
     deploy: {
         production: {
             user: 'node',
-            host: '52.15.127.7',
+            host: '172-31-1-42',
             ref: 'origin/master',
             repo: 'https://github.com/nikhilgarg459/iris.git',
             path: '/srv/production',
